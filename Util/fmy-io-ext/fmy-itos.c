5c89bffaa6e652e2626bbe1ba93549436614de70
fanmingyang <1056218492@qq.com>
2021-02-08 14:52:10 +0800

    update

--- Util/fmy-io-ext/fmy-itos.c
+++ Util/fmy-io-ext/fmy-itos.c
@@ -1,22 +1,38 @@
 #include "fmy-io.h"
 #include "fmy-string/fmy-string.h"
+#include <stddef.h>
 /**
  * 将int类型整数转换为一个字符串
  * 32位int最大数据需要10个字符，最小负数需要11个字符，还需要一个字符存储'\0'。
  * 因此，建议输入字符数组s的长度是12。
- */ 
+ */
 void myitos(int n, char s[])
 {
-    int i, sign;
-    if ((sign = n) < 0) /* record sign */
-        n = -n; /* make n positive */
-    i = 0;
-    do { /* generate digits in reverse order */
-        s[i++] = n % 10 + '0'; /* get next digit */
-    } while ((n /= 10) > 0); /* delete it */
-    if (sign < 0)
-        s[i++] = '-';
-    s[i] = '\0';
+    if (s == NULL)
+    {
+        return;
+    }
+
+    int sign = 0, index = 0;
+    if (n < 0)
+    {
+        sign = 1;
+        n = -n;
+    }
+
+    //因为一个整数数字至少有一位，所以可用do-while结构
+    do
+    {
+        s[index++] = n % 10 + '0';
+    } while ((n /= 10) > 0);
+
+    if (sign == 1)
+    {
+        s[index++] = '-';
+    }
+
+    s[index] = '\0';
+
     myreverse(s);
 
     return;
