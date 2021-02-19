a48e3226c07dc08fc99d3aafef497321bba091c0
fanmingyang <1056218492@qq.com>
2021-02-19 14:24:48 +0800

    add

---
+++ UnixIO/dir/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <dirent.h>
+
+int main() {
+    DIR * dir;
+    if (-1 == (dir = opendir("/home/fmy/CLionProjects/Pratice/"))) {
+        perror(strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    struct dirent * dirEntry;
+    while ((dirEntry = readdir(dir)) != NULL) {
+        fprintf(stdout, "name : %s\n", dirEntry->d_name);
+    }
+
+    if (-1 == closedir(dir)) {
+        perror(strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
