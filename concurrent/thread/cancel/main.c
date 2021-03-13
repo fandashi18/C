bafaa97d36f83b38e6f87e0e01f68bae63dd2e96
fanmingyang <1056218492@qq.com>
2021-03-13 18:00:47 +0800

    update format

--- concurrent/thread/cancel/main.c
+++ concurrent/thread/cancel/main.c
@@ -4,24 +4,25 @@
 #include <time.h>
 #include <stdlib.h>
 
-void * start_routine(void * arg);
+void *start_routine(void *arg);
 
 int main(int argc, char const *argv[])
 {
     pthread_t tid;
-    pthread_create(&tid,NULL,start_routine,NULL);
+    pthread_create(&tid, NULL, start_routine, NULL);
     struct timespec time = {.tv_nsec = 200};
-    nanosleep(&time,NULL);
+    nanosleep(&time, NULL);
     pthread_cancel(tid);
     exit(EXIT_SUCCESS);
 }
 
-void * start_routine(void * arg) {
+void *start_routine(void *arg)
+{
     for (int i = 0; i < 3; i++)
     {
-        printf("sleep %d s\n",i);
+        printf("sleep %d s\n", i);
         sleep(1);
     }
-    
-    return 0;//implicit call to pthread_exit()
+
+    return 0; //implicit call to pthread_exit()
 }
--- concurrent/thread/exit/main.c
+++ concurrent/thread/exit/main.c
@@ -6,24 +6,24 @@
 #include <errno.h>
 #include <stdlib.h>
 
-
-void * start_routine(void * arg);
+void *start_routine(void *arg);
 
 int main(int argc, char const *argv[])
 {
     pthread_t tid1;
-    tid1 = pthread_create(&tid1,NULL,start_routine,NULL);
+    tid1 = pthread_create(&tid1, NULL, start_routine, NULL);
 
     printf("main thread will exit.\n");
     pthread_exit(NULL);
 }
 
-void * start_routine(void * arg) {
+void *start_routine(void *arg)
+{
     for (int i = 0; i < 3; i++)
     {
-        printf("sleep %d s\n",i);
+        printf("sleep %d s\n", i);
         sleep(1);
     }
-    
-    return 0;//implicit call to pthread_exit()
+
+    return 0; //implicit call to pthread_exit()
 }
